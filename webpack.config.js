ec5cb309b2ca118ddf44e0db1448c34ffb3aeb0e
unknown <aquinasmoriasi@gmail.com>
2022-07-29 16:51:16 +0300

    add styling to page

--- webpack.config.js
+++ webpack.config.js
@@ -28,34 +28,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
-      },
     ],
   },
   performance: {
